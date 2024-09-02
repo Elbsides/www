cecad49d80b35e67e5526a5f1ed26bbb324697dd
Morton Swimmer <morton_swimmer@trendmicro.com>
2024-09-02 13:49:20 +0200

    create a generator script for schedule include and video file

--- 2024/includes/schedule.md
+++ 2024/includes/schedule.md
@@ -1,21 +1,21 @@
 | Time | Speaker | Title |
 | ---- | ------- | ----- |
-| 8:00	|		Registration	|
-| 9:00	|		Opening	|
-| 9:15	|		[Katharine Jarmul](#katharine-jarmul) | Opening keynote: [AI (In)security: How to (Still) Successfully Attack an AI System](#ai-insecurity-how-to-still-successfully-attack-an-ai-system)	|
-| 9:45	|		[Candid Wüest](#candid-wüest) | [Unveiling the Reality: The True Impact of GenAI on Cybercrime](#unveiling-the-reality-the-true-impact-of-genai-on-cybercrime)	|
-| 10:30	|		Break	|
-| 11:00	|		[Christian Kollee](#christian-kollee) | [Ready to Respond - Plan, Prepare, Try & Train](#ready-to-respond---plan-prepare-try--train)	|
-| 11:30	|		[Marc Ratfeld](#marc-ratfeld) | [Protecting the energy transition](#protecting-the-energy-transition)	|
-| 12:00	|		[Michael Helwig](#michael-helwig), [Álvaro Martínez](#álvaro-martínez) | [Help, my application is vulnerable, but how bad is it?](#help-my-application-is-vulnerable-but-how-bad-is-it)	|
-| 12:40	|		[Hilko Bengen](#hilko-bengen) | [Detection And Response for Linux without EDR](#detection-and-response-for-linux-without-edr)	|
-| 13:10	|		Lunch	|
-| 14:10	|		[Stefan Hager](#stefan-hager) | [Far beyond the perimeter - exploring external attack surfaces](#far-beyond-the-perimeter---exploring-external-attack-surfaces)	|
-| 14:40	|		[Timo Pagel](#timo-pagel) | [Key Steps to Achieving an Application Security Program](#key-steps-to-achieving-an-application-security-program)	|
-| 15:10	|		[Sam Macdonald](#sam-macdonald) | [Dealing With Imposter Syndrome](#dealing-with-imposter-syndrome)	|
-| 15:40	|		Break |
-| 16:10	|		[Max Maaß](#max-maaß), [Tim Walter](#tim-walter) | [Protectors of the Realm: Breaking and Fixing Keycloak Configurations](#protectors-of-the-realm-breaking-and-fixing-keycloak-configurations)	|
-| 16:40	|		[Paul Vixie](#paul-vixie) | [The evolution of cloud security](#the-evolution-of-cloud-security) |
-| 17:10	|		[Marit Hansen](#marit-hansen) | Closing keynote: [How to implement data protection – fulfilling the (not so new) requirement of the GDPR](#how-to-implement-data-protection--fulfilling-the-not-so-new-requirement-of-the-gdpr) |
-| 17:40	|		Closing	|
-| 18:00	|		Networking hour begins	|
+| 8:00 | | Registration |
+| 9:00 | (Morton Swimmer)[#morton] | Opening |
+| 9:15 | (Katharine Jarmul)[#katharine-jarmul] | Keynote: (AI (In)security: How to (Still) Successfully Attack an AI System)[#ai-insecurity-how-to-still-successfully-attack-an-ai-system] |
+| 9:45 | (Candid Wüest)[#candid-wüest] | (Unveiling the Reality: The True Impact of GenAI on Cybercrime)[#unveiling-the-reality-the-true-impact-of-genai-on-cybercrime] |
+| 10:30 | | Break |
+| 11:00 | (Christian Kollee)[#christian-kollee] | (Ready to Respond - Plan, Prepare, Try & Train)[ "#ready-to-respond---plan-prepare-try--train"] |
+| 11:30 | (Marc Ratfeld)[#marc-ratfeld] | (Protecting the energy transition)[#protecting-the-energy-transition] |
+| 12:00 | (Michael Helwig)[#michael-helwig], (Álvaro Martínez)[#álvaro-martínez] | (Help, my application is vulnerable, but how bad is it?)[#help-my-application-is-vulnerable-but-how-bad-is-it] |
+| 12:40 | (Hilko Bengen)[#hilko-bengen] | (Detection And Response for Linux without EDR)[#detection-and-response-for-linux-without-edr] |
+| 13:10 | | Lunch |
+| 14:10 | (Stefan Hager)[#stefan-hager] | (Far beyond the perimeter - exploring external attack surfaces)[#far-beyond-the-perimeter---exploring-external-attack-surfaces] |
+| 14:40 | (Timo Pagel)[#timo-pagel] | (Key Steps to Achieving an Application Security Program)[#key-steps-to-achieving-an-application-security-program] |
+| 15:10 | (Sam Macdonald)[#sam-macdonald] | (Dealing With Imposter Syndrome)[#dealing-with-imposter-syndrome] |
+| 15:40 | | Break |
+| 16:10 | (Max Maaß)[#max-maaß], (Tim Walter)[#tim-walter] | (Protectors of the Realm: Breaking and Fixing Keycloak Configurations)[#protectors-of-the-realm-breaking-and-fixing-keycloak-configurations] |
+| 16:40 | (Paul Vixie)[#paul-vixie] | (The evolution of cloud security)[#the-evolution-of-cloud-security] |
+| 17:10 | (Marit Hansen)[#marit-hansen] | Keynote: (How to implement data protection - fulfilling the (not so new) requirement of the GDPR)[#how-to-implement-data-protection--fulfilling-the-not-so-new-requirement-of-the-gdpr] |
+| 17:40 |  | Closing |
+| 18:00 | | Networking hour |
---
+++ convert_schedule.py
@@ -0,0 +1,104 @@
+import csv
+import os
+import sys
+
+
+def render_speakers(speakers, links=None):
+    if speakers == '':
+        return ''
+    if '|' in speakers:
+        speakers = speakers.split('|')
+        if links:
+            links = links.split('|')
+            return ', '.join([f"({speaker.strip()})[{link.strip()}]" for speaker, link in zip(speakers, links)])
+        else:
+            return ', '.join([f"{speaker.strip()}" for speaker in speakers])
+    if links:
+        return f"({speakers})[{links}]"
+    else:
+        return f"{speakers}"
+
+
+def ms_break_line(record):
+    return f"| {record['start']} | | {record['title']} |"
+
+def md_talk_line(record):
+    return f"| {record['start']} | {render_speakers(record['speaker'], record['speakerlink'])} | ({record['title']})[{record['titlelink']}] |"
+
+def md_administrivia_line(record):
+    return f"| {record['start']} | {render_speakers(record['speaker'], record['speakerlink'])} | {record['title']} |"
+
+def md_keynote_line(record):
+    return f"| {record['start']} | {render_speakers(record['speaker'], record['speakerlink'])} | Keynote: ({record['title']})[{record['titlelink']}] |"
+
+def main(input_file):
+    md_schedule = ["| Time | Speaker | Title |","| ---- | ------- | ----- |"]
+    video_schedule = []
+    directory = os.path.dirname(input_file)
+    file_name = os.path.splitext(os.path.basename(input_file))[0]
+    # print(directory, file_name)
+    with open(input_file, 'r', encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for record in list(reader):
+            # print(record)
+            if record["type"] == "talk":
+                md_schedule.append(md_talk_line(record))
+            elif record["type"] == "keynote":
+                md_schedule.append(md_keynote_line(record))
+            elif record["type"] == "break":
+                md_schedule.append(ms_break_line(record))
+            elif record["type"] == "administrivia":
+                md_schedule.append(md_administrivia_line(record))
+
+            if record["type"] == "administrivia" or record["type"] == "talk" or record["type"] == "keynote":
+                title_lines = []
+                title = record['title']
+                while len(title) > 40:
+                    split_index = title[:40].rfind(' ')
+                    if split_index == -1:
+                        split_index = 40
+                    title_lines.append(title[:split_index].strip())
+                    title = title[split_index:].strip()
+                title_lines.append(title)
+
+                start_time = record['start']
+                duration = record['duration']
+                start_hour, start_minute = map(int, start_time.split(':'))
+                duration_hour, duration_minute = map(int, duration.split(':'))
+
+                end_hour = start_hour + duration_hour
+                end_minute = start_minute + duration_minute
+
+                if end_minute >= 60:
+                    end_hour += 1
+                    end_minute -= 60
+
+                end_time = f"{end_hour:02d}:{end_minute:02d}"
+                start_to_end = f"{record['start']}-{end_time}"
+                video_schedule += [f"{start_to_end}\t{title_line}" for title_line in title_lines]
+                video_schedule.append(f"{start_to_end}\t - {render_speakers(record['speaker'])}")
+                video_schedule.append("")
+
+    with open(os.path.join(directory, f"{file_name}.md"), 'w', encoding="utf-8") as f:
+        f.write("\n".join(md_schedule))
+
+    # print("\n".join(video_schedule))
+    with open(os.path.join(directory, f"{file_name}.txt"), 'w', encoding="utf-8") as f:
+        f.write("\n".join(video_schedule))
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+        if not os.path.exists(input_file):
+            print(f"File {input_file} does not exist.")
+            sys.exit(1)
+        if not os.path.isfile(input_file):
+            print(f"{input_file} is not a file.")
+            sys.exit(1)
+        if not os.path.splitext(input_file)[1] == '.csv':
+            print("Please provide a CSV file as input.")
+            sys.exit(1)
+    else:
+        print("Please provide the input file path as a command line argument.")
+        sys.exit(1)
+    main(input_file)
